52231bd7c9fdff2fa73c0edab008fcbb0c01cd54
willi <willi@LAPTOP-NGU55PT5>
2025-03-20 14:58:08 -0300

    validação de dados - nulo

--- MauiAppMinhasCompras/Models/Produto.cs
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -5,11 +5,24 @@ namespace MauiAppMinhasCompras.Models
 {
     public class Produto
     {
+        string _descricao;
+
         internal object Nome;
 
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        public string Descricao { get; set; }
+        public string Descricao { 
+            get => _descricao; 
+            set
+            {
+                if(value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+                _descricao = value;
+            }
+        
+        }
         public double Quantidade { get; set; }
         public double Preco {  get; set; }
 
